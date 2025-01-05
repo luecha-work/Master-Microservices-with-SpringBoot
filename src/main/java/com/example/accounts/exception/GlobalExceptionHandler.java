229ac7a7e906f7643fe9f55857b8547544094694
luecha.kan <luecha.kanm@gmail.com>
2025-01-05 19:10:38 +0700

    cleanup code

--- src/main/java/com/example/accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/accounts/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.example.accounts.exception;
 
 import com.example.accounts.dto.ErrorResponseDto;
-import com.example.accounts.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
