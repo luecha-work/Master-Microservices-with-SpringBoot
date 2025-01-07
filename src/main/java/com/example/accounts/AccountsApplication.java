76d0e0b4eacce988ec52c024f931438fd57bda8f
luecha.kan <luecha.kanm@gmail.com>
2025-01-07 22:54:07 +0700

    Introduction to documentation of REST APIs using springdoc openapi

--- src/main/java/com/example/accounts/AccountsApplication.java
+++ src/main/java/com/example/accounts/AccountsApplication.java
@@ -1,5 +1,7 @@
 package com.example.accounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(info = @Info(title = "Account Service API", version = "v0.1"))
 public class AccountsApplication {
 
     public static void main(String[] args) {
