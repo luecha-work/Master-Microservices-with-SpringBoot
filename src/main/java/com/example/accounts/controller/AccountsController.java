6e6b9607328bd9f9795f341b717c703cae3f0e15
luecha.kan <luecha.kanm@gmail.com>
2025-01-07 23:22:48 +0700

    Enhancing documentation of REST APIs using @Tag, @Operation, @ApiResponse

--- src/main/java/com/example/accounts/controller/AccountsController.java
+++ src/main/java/com/example/accounts/controller/AccountsController.java
@@ -4,6 +4,10 @@ import com.example.accounts.constants.AccountsConstants;
 import com.example.accounts.dto.CustomerDto;
 import com.example.accounts.dto.ResponseDto;
 import com.example.accounts.sevice_contract.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -13,6 +17,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "CRUD REST APIs for Account",
+        description = "CRUD REST API for Accounts in Workshop EazyBank")
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
@@ -25,6 +32,19 @@ public class AccountsController {
 //        this.accountsService = accountsService;
 //    }
 
+    @Operation(
+            summary = "Create Account REST API",
+            description = "Create a new account for a customer")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Account created successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Request"
+            )
+    })
     @PostMapping("/account")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         accountsService.createAccounts(customerDto);
@@ -32,15 +52,39 @@ public class AccountsController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Get Account Detail REST API",
+            description = "Get account details for a customer")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Account details fetched successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Account not found"
+            )
+    })
     @GetMapping("/account")
-    public ResponseEntity<CustomerDto> getAccountDetail(
-            @RequestParam
-            @Pattern(regexp = "^$|[0-9]{10}", message = "Mobile number must be 10 digits") String mobileNumber) {
+    public ResponseEntity<CustomerDto> getAccountDetail(@RequestParam @Pattern(regexp = "^$|[0-9]{10}", message = "Mobile number must be 10 digits") String mobileNumber) {
         CustomerDto customerDto = accountsService.getAccountDetailByMobilePhone(mobileNumber);
 
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 
+    @Operation(
+            summary = "Update Account REST API",
+            description = "Update account details for a customer")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Account updated successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal Server Error"
+            )
+    })
     @PatchMapping("/account")
     public ResponseEntity<ResponseDto> updateAccount(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = accountsService.updateAccount(customerDto);
@@ -51,6 +95,19 @@ public class AccountsController {
         return ResponseEntity.status(status).body(new ResponseDto(statusCode, message));
     }
 
+    @Operation(
+            summary = "Delete Account REST API",
+            description = "Delete account for a customer")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Account deleted successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal Server Error"
+            )
+    })
     @DeleteMapping("/account")
     public ResponseEntity<ResponseDto> deleteAccount(@RequestParam String mobileNumber) {
         boolean isDeleted = accountsService.deleteAccount(mobileNumber);
